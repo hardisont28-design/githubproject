96bab98ff82017a13e3e110376ed3dc98437e56c
Evan Hammond <hammonde28@stxtigers.com>
2025-11-14 11:57:22 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-
 let mySprite: Sprite = null
+let mySprite2: Sprite = null
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite = sprites.createProjectileFromSide(img`
         2 8 8 8 8 8 2 8 8 8 8 8 2 8 8 8 
@@ -19,4 +19,24 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         8 8 8 8 2 8 8 8 8 8 2 8 8 8 8 8 
         2 8 8 8 8 8 2 8 8 8 8 8 2 8 8 8 
         `, 50, 50)
+})
+forever(function () {
+    mySprite2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . b b b b b 5 5 5 5 5 5 5 b . . 
+        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+        . . b d 5 5 b 1 f f 5 4 4 c . . 
+        b b d b 5 5 5 d f b 4 4 4 4 4 b 
+        b d d c d 5 5 b 5 4 4 4 4 4 b . 
+        c d d d c c b 5 5 5 5 5 5 5 b . 
+        c b d d d d d 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `, SpriteKind.Player)
 })
